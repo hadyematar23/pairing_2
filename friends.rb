249cf82c0f259aabf307b15d0cae080a86f43160
Hady Matar <hadyematar@gmail.com>
2022-10-19 20:33:31 -0600

    Add if statement

--- friends.rb
+++ friends.rb
@@ -1,6 +1,12 @@
 best_friend = 'Molly'
+other_friend = 'Tracy'
 if best_friend
     puts "you are my best friend"
+elsif other_friend 
+    puts "hey, you're not molly"
 else 
     puts "you are a leecher who wants money"
 end 
+
+
+
